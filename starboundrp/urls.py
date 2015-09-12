03f7245f0c61bdd4189bdb1849d8c0087cadd8a1
Amsel <>
2015-09-12 17:56:58 +0200

    add simple character list view

--- starboundrp/urls.py
+++ starboundrp/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from character.views import CharacterList
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^characters/$', CharacterList.as_view())
 ]
