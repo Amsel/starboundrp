b8a69fe4731cfe111f1b0dcc07f702a0e23c4b4b
Amsel <>
2015-09-19 19:32:25 +0200

    add dynamic urls to templates

--- character/urls.py
+++ character/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import url
 from .views import CharacterDetail, CharacterList
 
 urlpatterns= [
-    url(r'^$', CharacterList.as_view(), name='characters'),
+    url(r'^$', CharacterList.as_view(), name='character_list'),
     url(r'^(?P<pk>[0-9]+)/$', CharacterDetail.as_view(), name='character_detail'),
 ]
