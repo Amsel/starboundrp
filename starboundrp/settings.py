0bd390df8659c83279ddbecae7a5e38a54cca91a
Amsel <>
2015-09-20 17:33:34 +0200

    add basic setup for global static files

--- starboundrp/settings.py
+++ starboundrp/settings.py
@@ -56,8 +56,7 @@ ROOT_URLCONF = 'starboundrp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -102,3 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
