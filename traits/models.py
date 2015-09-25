6cb33d8580c630c33f49a6708dc0d34351827c96
Amsel <>
2015-09-25 19:12:32 +0200

    first draft implementation of character traits
    - the conflicts are not handled n a nice way
    - this will not live inside the character app, since traits are optional
    - this will include a "traitset" proxy, handling further constraints on combinations
    - maybe to be meaningfull and avoid the character app to depend on this, a global mechanism to display a character is needed

--- traits/models.py
+++ traits/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+
+class Trait(models.Model):
+    name = models.CharField(max_length=127)
+    description = models.TextField()
+
+    # the number of slots it uses
+    weight = models.IntegerField(default=1)
+    bonus = models.BooleanField(default=False)
+
+    # The trait requires other traits
+    depends_on = models.ManyToManyField('Trait',
+                                        related_name='trait_depends_on',
+                                        blank=True, default=None)
+    # The trait conflicts with other traits
+    conflicts_with = models.ManyToManyField('Trait',
+                                            related_name='trait_conflicts_with',
+                                            blank=True, default=None)
+
+    characters = models.ManyToManyField('character.Character', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def is_bonus(self):
+        return self.bonus
+
+    def is_malus(self):
+        return not self.bonus
