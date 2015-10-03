184d3112923fed9fea127bd9c2339daa3e567f15
Amsel <>
2015-10-03 22:20:54 +0200

    [traits] make traits app add functions to the character admin page
     - The character is now almost completely uncoupled from the traits,
       only thing remaining is the template tag, but removing this does
       not make much sense. maybe there is a usefull way to use
       global templates to even remove this dependency

--- character/admin.py
+++ character/admin.py
@@ -2,14 +2,12 @@ from django.contrib import admin
 
 from .models import *
 
-from traits.admin import TraitInline
-
 # Register your models here.
 
 
 @admin.register(Character)
 class CharacterAdmin(admin.ModelAdmin):
-    inlines = [TraitInline]
+    pass
 
 
 admin.site.register(Species)
--- traits/admin.py
+++ traits/admin.py
@@ -1,16 +1,19 @@
 from django.contrib import admin
 
-from .models import Trait
+from character.admin import CharacterAdmin
 
+from .models import Trait
 # Register your models here.
 
 
 @admin.register(Trait)
 class TraitAdmin(admin.ModelAdmin):
-    exclude = ('characters',)
+    exclude = ('characters', 'trait_inline')
+    inlines = []  # empty list, so the Trait inline is not inserted by default
 
 
 class TraitInline(admin.StackedInline):
     model = Trait.characters.through
     extra = 1
 
+CharacterAdmin.inlines.append(TraitInline)
